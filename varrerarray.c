43d13383b5ecb024a899636b417895acedcaa3f3
Victor Iorio <75426069+Vikstar1856@users.noreply.github.com>
2025-06-17 21:20:54 -0300

    Add files via upload

---
+++ varrerarray.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+int notas[5];
+notas[0] = 1;
+notas[1] = 4;
+notas[2] = 7;
+notas[3] = 5;
+notas[4] = 10;
+
+for (int i = 0; i < 5; i++) {
+    printf("%d e %d\n", notas[i], i); 
+}
+}
