7d94e66c738b644ef7b98d80c54cae5a4950c25c
Victor Iorio <75426069+Vikstar1856@users.noreply.github.com>
2025-11-15 16:05:38 -0300

    Update cadastro.c
    Adicionado funcionalidade de busca por nome

--- cadastro.c
+++ cadastro.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
 FILE *fptr;
@@ -39,6 +40,44 @@ fptr = fopen("cadastros.txt", "w");
     }
 fclose(fptr);
     printf("\nArquivo 'cadastros.txt' criado com sucesso!");
+ printf("\n-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-\n");
+    fptr = fopen("cadastros.txt", "r");
+    char nome_busca[30];
+    int loopar;
+do {
+    int encontrou = 0;
+    rewind(fptr);
+    printf("\nPesquisa por nome.\n");
+    printf("Digite o nome a procurar no arquivo:");
+    scanf("%s", nome_busca);
 
-
+    while (fscanf(fptr, "%s %d %d %d  %d", nome, &idade, &dia, &mes, &ano) == 5) {
+        if (strcmp(nome, nome_busca) == 0) {
+            printf("Registro encontrado no sistema!\n");
+            printf("Nome: %s\n", nome);
+            printf("Idade: %d\n", idade);
+            printf("Nascimento: %d/%d/%d\n", dia, mes, ano);
+            encontrou = 1;
+            break;
+        }
+        }
+    if (encontrou == 0) {
+        printf("Nao foi possivel localizar o nome no banco de dados, deseja realizar a consulta novamente?\n");
+        printf("[1] Sim | [2] Nao\n");
+        scanf("%d", &loopar);
+        if (loopar == 2) {
+            printf("Encerrando...");
+            break;
+        }
+    }
+    if (encontrou == 1) {
+    printf("Deseja realizar a consulta novamente?\n");
+        printf("[1] Sim | [2] Nao\n");
+        scanf("%d", &loopar);
+        if (loopar == 2) {
+            printf("Encerrando...");
+            break;
+        }
+    }
+}while (loopar == 1);
 }
