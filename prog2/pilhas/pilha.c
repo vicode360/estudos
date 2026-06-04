80de5c7d70b798082f4c166fd0c64392a5d8146c
vicode360 <victor.s.iorio@gmail.com>
2026-06-04 18:34:06 -0300

    pilhas

---
+++ prog2/pilhas/pilha.c
@@ -0,0 +1,24 @@
+#define MAXSIZE 100
+#include <stdio.h>
+
+typedef struct {
+    int stack[MAXSIZE];
+    int top;
+} Stack;
+
+
+
+int main() {
+    //     int stack[MAXSIZE];
+    //     int n = 0;
+    //     int x = 0;
+    //     for (int i = 0; i < MAXSIZE; i++) {
+    //         stack[n++] = x;
+    //         x++;
+    //     }
+    //    while (n > 0) {
+    //        printf("%d\n", n);
+    //        n--;
+    //    }
+    //
+}
